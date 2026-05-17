f358d2cfdc76957307f29624d74c0b5d84a9a720
jason Anine <139896037+jasonanine0@users.noreply.github.com>
2026-05-17 11:02:30 -0400

    fix: use generic eslint-disable for require() in flutterwave-service

--- src/lib/flutterwave-service.ts
+++ src/lib/flutterwave-service.ts
@@ -6,7 +6,7 @@ let flw: any = null;
 // Initialize Flutterwave only on server side
 if (typeof window === 'undefined') {
   try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    // eslint-disable-next-line
     const Flutterwave = require('flutterwave-node-v3');
     flw = new Flutterwave(
       process.env.FLUTTERWAVE_PUBLIC_KEY!,
